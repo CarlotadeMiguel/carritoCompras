0666cbf3f337507056264653540608d0ddc1d95f
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-04-30 19:53:29 +0200

    añadir más productos

--- src/components/ProductosList.js
+++ src/components/ProductosList.js
@@ -7,7 +7,13 @@ import Producto from './Producto';
 const productos = [
   { id: 1, nombre: 'Camiseta', precio: 20, imagen: 'https://picsum.photos/150' },
   { id: 2, nombre: 'Pantalón', precio: 40, imagen: 'https://picsum.photos/150' },
-  { id: 3, nombre: 'Zapatos', precio: 60, imagen: 'https://picsum.photos/150' }
+  { id: 3, nombre: 'Zapatos', precio: 60, imagen: 'https://picsum.photos/150' },
+  { id: 4, nombre: 'Camisa', precio: 10, imagen: 'https://picsum.photos/150' },
+  { id: 5, nombre: 'Pantalones', precio: 30, imagen: 'https://picsum.photos/150' },
+  { id: 6, nombre: 'Zapatillas', precio: 50, imagen: 'https://picsum.photos/150' },
+  { id: 7, nombre: 'Blusa', precio: 70, imagen: 'https://picsum.photos/150' },
+  { id: 8, nombre: 'Falda', precio: 80, imagen: 'https://picsum.photos/150' },
+  { id: 9, nombre: 'Bufanda', precio: 5, imagen: 'https://picsum.photos/150' }
 ];
 
 const ProductosList = () => {
